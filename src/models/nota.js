654274ac55ee6b2e50881209dace79c1c1b3a386
Amanda Viória <amandavic7576@gmail.com>
2023-06-14 11:06:11 -0300

    media CA

--- src/models/nota.js
+++ src/models/nota.js
@@ -1,22 +1,34 @@
 class Nota {
     constructor(disciplina, a1, a2, a3) {
-        this.disciplina = disciplina;
-        this.a1 = a1;
-        this.a2 = a2;
-        this.a3 = a3;
+      this.disciplina = disciplina;
+      this.a1 = a1;
+      this.a2 = a2;
+      this.a3 = a3;
     }
-
+  
     mediaFinal() {
-        return Math.max(
-            0.4 * this.a1 + 0.6 * this.a2, 
-            0.4 * this.a1 + 0.6 * this.a3, 
-            0.4 * this.a3 + 0.6 * this.a2
-        );
+      return Math.max(
+          0.4 * this.a1 + 0.6 * this.a2, 
+          0.4 * this.a1 + 0.6 * this.a3, 
+          0.4 * this.a3 + 0.6 * this.a2
+      );
+  }
+  
+    mediaCA(media = this.mediaFinal()) {
+      if (media >= 9.0) {
+        return "SS - Superior"; // superior
+      } else if (media >= 7.0 && media <= 8.99) {
+        return "MS - Médio Superior"; // médio superior
+      } else if (media >= 5.0 && media <= 6.99) {
+        return "MM - Médio"; // médio
+      } else if (media >= 4.0 && media <= 4.99) {
+        return "MI - Médio Inferior"; // médio inferior
+      } else if (media >= 0.1 && media <= 2.99) {
+        return "II - Inferior"; // inferior
+      } else {
+        return "SR - Sem rendimento"; // sem rendimento
+      }
     }
-
-    mediaCA() {
-        return "SS";
-    }
-}
-
-module.exports = Nota;
+  }
+  
+  module.exports = Nota;
